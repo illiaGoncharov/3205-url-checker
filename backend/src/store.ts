41b74cce1f521ba16d76d6eb025724b0f301c052
Illia Goncharov <ilia.m.goncharov@gmail.com>
2026-08-16 18:26:21 +0300

    feat: store jobs in memory

--- backend/src/store.ts
+++ backend/src/store.ts
@@ -1,1 +1,16 @@
 // Задания в памяти: Map по jobId
+import type { Job } from './types';
+
+// Map заданий
+export const jobs = new Map<string, Job>();
+
+// Получить задание по id
+export const getJob = (id: string): Job | undefined => jobs.get(id);
+
+// Установить задание
+export const setJob = (job: Job): void => {
+  jobs.set(job.id, job);
+};
+
+// Получить все задания
+export const getAllJobs = (): Job[] => Array.from(jobs.values());
